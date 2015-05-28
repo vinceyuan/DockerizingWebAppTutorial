ccaaa84853c1342e852ebbb0e35afc4cb7a2525e
Vince Yuan <vince.yuan@gmail.com>
2015-05-28 21:34:11 +0800

    Added mynodeapp

---
+++ mynodeapp/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var pg = require('pg');
+var redis = require("redis");
+var redisClient = redis.createClient(6379, '127.0.0.1', {})
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var conString = "postgres://vince:@localhost/mynodeappdb"; // Use your db, user and password
+var pgClient = new pg.Client(conString);
+pgClient.connect(function(err) {
+  if(err) return console.error('Could not connect to postgres', err);
+  console.log('Connected to postgres');
+});
+
+app.get('/', function(req, res) {
+	pgClient.query('SELECT NOW() AS "theTime"', function(err1, result) {
+    redisClient.get("number", function(err2, reply) {
+	    res.render('index', { pgTime: result.rows[0].theTime, number: reply });
+		});
+  }); // Errors Ignored in this example. You should check errors in a real project. 
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
